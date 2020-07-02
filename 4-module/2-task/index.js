1d971c802a35019a96256447c0005985bece11e3
alidmi <alile@ngs.ru>
2020-07-02 18:24:45 +0700

    admitrieva: 4th module task 2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,10 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  let rowsNum = table.rows.length;
+
+  for (let i = 0; i < rowsNum; i++) {
+    let row = table.rows[i];
+    row.cells[i].style.backgroundColor = 'red';
+  }
 }
