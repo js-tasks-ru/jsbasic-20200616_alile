86e50f125caed361f69db82eac2b1a3a0492852b
alidmi <alile@ngs.ru>
2020-07-29 12:48:41 +0700

    admitrieva: 5th module tasks 1,2, remove debug messages

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -2,8 +2,6 @@ function toggleText() {
   const button = document.querySelector('.toggle-text-button')
   const text = document.getElementById('text')
   button.addEventListener('click', event => {
-      console.log(text.getAttribute('hidden'));
-      console.log(text);
       if (text.getAttribute('hidden') == 'true') {
         text.removeAttribute('hidden');
       } else {
